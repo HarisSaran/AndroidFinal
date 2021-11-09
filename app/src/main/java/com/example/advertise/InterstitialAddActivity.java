253b699fee6573acea75abbe3a9144cd1a96ec16
Haris Saran <harissaran33@gmail.com>
2021-11-09 10:47:36 -0600

    IntersitialAdd is complete

---
+++ app/src/main/java/com/example/advertise/InterstitialAddActivity.java
@@ -0,0 +1,14 @@
+package com.example.advertise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InterstitialAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interstitial_add);
+    }
+}
--- app/src/main/java/com/example/advertise/MainActivity.java
+++ app/src/main/java/com/example/advertise/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.advertise;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 public class MainActivity extends AppCompatActivity {
+    // Banner Add
     private AdView mAdView;
+    // Interstitial Add
+    private InterstitialAd mInterstitialAd;
+    private Button btn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn_interstitial);
 
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
@@ -23,9 +40,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // BANNER Add
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        // INTERSTITIAL Add
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        mInterstitialAd = interstitialAd;
+//                        Log.i(TAG, "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+//                        Log.i(TAG, loadAdError.getMessage());
+                        mInterstitialAd = null;
+                    }
+                });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mInterstitialAd != null) {
+                    mInterstitialAd.show(MainActivity.this);
+//                    Intent intent = new Intent(MainActivity.this, InterstitialAddActivity.class);
+//                    startActivity(intent);
+                } else {
+                    Log.d("TAG", "The interstitial ad wasn't ready yet.");
+                }
+            }
+        });
     }
 }
+
+
+
+
