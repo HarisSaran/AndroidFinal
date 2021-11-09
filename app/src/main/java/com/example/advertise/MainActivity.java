ee075c4b12f4fe97285498d055f0fa6000a6f94a
Haris Saran <harissaran33@gmail.com>
2021-11-09 11:33:58 -0600

    implimented rewarded adds

--- app/src/main/java/com/example/advertise/MainActivity.java
+++ app/src/main/java/com/example/advertise/MainActivity.java
@@ -3,28 +3,36 @@ package com.example.advertise;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
 public class MainActivity extends AppCompatActivity {
     // Banner Add
     private AdView mAdView;
     // Interstitial Add
     private InterstitialAd mInterstitialAd;
-    private Button btn;
+    private Button btnInterstitial;
+    // Rewarded Add
+    private RewardedAd mRewardedAd;
+    private final String TAG = "MainActivity";
+    private Button btnRewards;
 
 
     @Override
@@ -32,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btn_interstitial);
+        btnInterstitial = findViewById(R.id.btn_interstitial);
+        btnRewards = findViewById(R.id.btn_rewards);
 
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
@@ -53,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                         // The mInterstitialAd reference will be null until
                         // an ad is loaded.
                         mInterstitialAd = interstitialAd;
-//                        Log.i(TAG, "onAdLoaded");
+                        Log.i(TAG, "onAdLoaded");
                     }
 
                     @Override
@@ -64,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnInterstitial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (mInterstitialAd != null) {
@@ -76,6 +85,55 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // REWARD Add
+//        AdRequest adRequest = new AdRequest.Builder().build();
+        // MY NUMBER RewardedAd.load(this, "ca-app-pub-5550814697393931/7694046869",
+        RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917",
+                adRequest, new RewardedAdLoadCallback() {
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error.
+                        Log.d(TAG, loadAdError.getMessage());
+                        mRewardedAd = null;
+                    }
+
+                    @Override
+                    public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                        mRewardedAd = rewardedAd;
+                        Log.d(TAG, "Ad was loaded.");
+                    }
+                });
+        // Here we load the adds on button click
+        btnRewards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DisplayRewardedAds();
+            }
+        });
+    }
+
+    private void DisplayRewardedAds(){
+        // directs us to the next activity and displays a reward video
+        Intent i = new Intent(this, InterstitialAddActivity.class);
+        startActivity(i);
+
+        // display the add
+        if (mRewardedAd != null) {
+            Activity activityContext = MainActivity.this;
+            mRewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
+                @Override
+                public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                    // Handle the reward.
+                    int rewardAmount = rewardItem.getAmount();
+                    String rewardType = rewardItem.getType();
+
+                    Log.d(TAG, "The user earned." + rewardAmount);
+                }
+            });
+        } else {
+            Log.d(TAG, "The rewarded ad wasn't ready yet.");
+        }
     }
 }
 
