0b522a335737a92147a3f3974d859bfae39cb670
Haris Saran <harissaran33@gmail.com>
2021-11-27 11:31:47 -0600

    add the databasehelper.java and created a database

--- app/src/main/java/com/example/advertise/AuthenticationActivity.java
+++ app/src/main/java/com/example/advertise/AuthenticationActivity.java
@@ -11,6 +11,7 @@ import com.amazonaws.mobile.client.Callback;
 import com.amazonaws.mobile.client.SignInUIOptions;
 import com.amazonaws.mobile.client.UserStateDetails;
 
+// Handle our Login
 public class AuthenticationActivity extends AppCompatActivity {
 
     private final String TAG = AuthenticationActivity.class.getSimpleName();
@@ -20,11 +21,16 @@ public class AuthenticationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
 
+        // Initialize our AWS mobile SDK
         AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
 
+            // OnResult method gives us the user state details
             @Override
             public void onResult(UserStateDetails userStateDetails) {
                 Log.i(TAG, userStateDetails.getUserState().toString());
+
+                // When we have the user state we figure out if the user is
+                // signed in, signed out or some other state..
                 switch (userStateDetails.getUserState()){
                     case SIGNED_IN:
                         Intent i = new Intent(AuthenticationActivity.this, MainActivity.class);
@@ -47,6 +53,8 @@ public class AuthenticationActivity extends AppCompatActivity {
         });
     }
 
+    // If they are signed in pass them on to the MainActivity
+    // or another activity within the app!
     private void showSignIn() {
         try {
             AWSMobileClient.getInstance().showSignIn(this,
